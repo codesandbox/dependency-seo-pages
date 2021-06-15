c93be708d59a9c6de4f271d2f8304e2c0289902b
Danilo Woznica <danilowoz@gmail.com>
2021-06-15 14:20:12 +0100

    feat(build): add sentry

---
+++ sentry.server.config.js
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://7effd3c3ebe34ec2b7e2420e50563c3a@o72233.ingest.sentry.io/5817864',
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
